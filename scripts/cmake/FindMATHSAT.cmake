232e64d97ce24a40537705daa7c911f12bc62a29
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2020-06-06 16:41:16 -0400

    MathSAT doesn't actually require static linking to pthread
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- scripts/cmake/FindMATHSAT.cmake
+++ scripts/cmake/FindMATHSAT.cmake
@@ -5,13 +5,6 @@ function(check_mathsat_version mathsat_include mathsat_lib)
   # Get lib path
   get_filename_component(mathsat_lib_path ${mathsat_lib} PATH)
 
-  if (NOT BUILD_SHARED_LIBS)
-    find_package(Threads REQUIRED)
-    if(Threads_FOUND)
-      set(mathsat_lib "${mathsat_lib} ${CMAKE_THREAD_LIBS_INIT} -ldl")
-    endif()
-  endif ()
-
   try_run(
     MATHSAT_RETURNCODE
     MATHSAT_COMPILED
@@ -41,14 +34,6 @@ if(SOLVER_MATHSAT_INCLUDE_DIR AND SOLVER_MATHSAT_LIB)
   # a small C++ program to detect it via the MATHSAT_get_version() API call.
   check_mathsat_version(${SOLVER_MATHSAT_INCLUDE_DIR} ${SOLVER_MATHSAT_LIB})
 endif()
-
-# Alright, now create a list with mathsat and it's dependencies
-if (NOT BUILD_SHARED_LIBS)
-  find_package(Threads REQUIRED)
-  if(Threads_FOUND)
-    set(SOLVER_MATHSAT_LIB "${SOLVER_MATHSAT_LIB} ${CMAKE_THREAD_LIBS_INIT} -ldl")
-  endif()
-endif ()
 
 # handle the QUIETLY and REQUIRED arguments and set MATHSAT_FOUND to TRUE if
 # all listed variables are TRUE
