318c8e9274761cdf82fb518ebde27169540275ff
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2020-06-07 20:26:31 -0400

    Fix mathsat test program
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- scripts/cmake/try_mathsat.cpp
+++ scripts/cmake/try_mathsat.cpp
@@ -1,7 +1,7 @@
 #include <mathsat.h>
 
 int main() {
-  const char *msat_version = msat_get_version();
+  char *msat_version = msat_get_version();
   printf("%s", msat_version);
   msat_free(msat_version);
   return 0;
