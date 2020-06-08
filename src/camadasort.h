79f59b2932b05e9c137a1d081da6c45b78afc162
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2020-06-08 19:09:02 -0400

    Templatefy types of sorts
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- src/camadasort.h
+++ src/camadasort.h
@@ -95,13 +95,13 @@ protected:
   virtual unsigned getFloatSortSizeImpl() const = 0;
 };
 
-/// Shared pointer for SMTSorts, used by SMTSolver API.
-using SMTSortRef = std::shared_ptr<SMTSort>;
-
 /// Template to hold Solver specific Context and Sort
 template <typename SolverContextRef, typename TheSort>
 class SolverSort : public SMTSort {
 public:
+  typedef SolverContextRef ContextType;
+  typedef TheSort SortType;
+
   SolverContextRef Context;
 
   TheSort Sort;
@@ -111,20 +111,109 @@ public:
 
   virtual ~SolverSort() = default;
 
-  virtual bool isBitvectorSortImpl() const = 0;
+  virtual bool isBitvectorSortImpl() const {
+    abortWithMessage("Unimplemented for current type");
+  }
 
-  virtual bool isBooleanSortImpl() const = 0;
+  virtual bool isBooleanSortImpl() const {
+    abortWithMessage("Unimplemented for current type");
+  }
 
-  virtual bool isFloatSortImpl() const = 0;
+  virtual bool isFloatSortImpl() const {
+    abortWithMessage("Unimplemented for current type");
+  }
 
-  virtual bool isRoundingModeSortImpl() const = 0;
+  virtual bool isRoundingModeSortImpl() const {
+    abortWithMessage("Unimplemented for current type");
+  }
 
-  virtual unsigned getBitvectorSortSizeImpl() const = 0;
+  virtual unsigned getBitvectorSortSizeImpl() const {
+    abortWithMessage("Unimplemented for current type");
+  }
 
-  virtual unsigned getFloatSortSizeImpl() const = 0;
+  virtual unsigned getFloatSortSizeImpl() const {
+    abortWithMessage("Unimplemented for current type");
+  }
 
   virtual bool equal_to(SMTSort const &Other) const = 0;
 };
+
+template <typename SolverSortBase> class SolverBVSort : public SolverSortBase {
+public:
+  unsigned Width;
+
+  SolverBVSort(unsigned W, typename SolverSortBase::ContextType C,
+               typename SolverSortBase::SortType S)
+      : Width(W), SolverSortBase(C, S){};
+  virtual ~SolverBVSort() = default;
+
+  virtual bool isBitvectorSortImpl() const { return true; }
+
+  virtual unsigned getBitvectorSortSizeImpl() const { return Width; }
+
+  virtual bool equal_to(SMTSort const &Other) const {
+    return SolverSortBase::equal_to(Other);
+  }
+};
+
+template <typename SolverSortBase>
+class SolverBoolSort : public SolverSortBase {
+public:
+  SolverBoolSort(typename SolverSortBase::ContextType C,
+                 typename SolverSortBase::SortType S)
+      : SolverSortBase(C, S){};
+  virtual ~SolverBoolSort() = default;
+
+  virtual bool isBooleanSortImpl() const { return true; }
+
+  virtual unsigned getBitvectorSortSizeImpl() const { return 1; }
+
+  virtual bool equal_to(SMTSort const &Other) const {
+    return SolverSortBase::equal_to(Other);
+  }
+};
+
+template <typename SolverSortBase> class SolverFPSort : public SolverSortBase {
+public:
+  unsigned ExpWidth;
+  unsigned SigWidth;
+
+  SolverFPSort(unsigned EW, unsigned SW, typename SolverSortBase::ContextType C,
+               typename SolverSortBase::SortType S)
+      : ExpWidth(EW), SigWidth(SW), SolverSortBase(C, S){};
+  virtual ~SolverFPSort() = default;
+
+  virtual bool isFloatSortImpl() const { return true; }
+
+  virtual unsigned getFloatSortSizeImpl() const { return ExpWidth + SigWidth; }
+
+  virtual bool equal_to(SMTSort const &Other) const {
+    return SolverSortBase::equal_to(Other);
+  }
+};
+
+template <typename SolverSortBase> class SolverRMSort : public SolverSortBase {
+public:
+  SolverRMSort(typename SolverSortBase::ContextType C,
+               typename SolverSortBase::SortType S)
+      : SolverSortBase(C, S){};
+  virtual ~SolverRMSort() = default;
+
+  virtual bool isRoundingModeSortImpl() const { return true; }
+
+  virtual bool equal_to(SMTSort const &Other) const {
+    return SolverSortBase::equal_to(Other);
+  }
+};
+
+/// Shared pointer for SMTSorts, used by SMTSolver API.
+using SMTSortRef = std::shared_ptr<SMTSort>;
+
+/// Wrapper to downcast from SMTSort to Solver specific sort
+template <typename SolverSort>
+static inline const SolverSort &toSolverSort(const SMTSort &S) {
+  return dynamic_cast<const SolverSort &>(S);
+}
 
 } // namespace camada
 
