76a61d20d260844cd79ceb6f365d7392bbf78e10
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2020-06-07 20:28:13 -0400

    Bump minimum version of Z3 and CVC4
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- scripts/cmake/FindCVC4.cmake
+++ scripts/cmake/FindCVC4.cmake
@@ -2,11 +2,10 @@
 find_package(CVC4 PATHS ${SOLVER_CVC4_DIR}/lib/cmake/CVC4 $ENV{HOME}/cvc4)
 
 if(CVC4_FOUND)
-
   # Remove any suffix from CVC4's version string
   string(REGEX REPLACE "([0-9]\\.[0-9]).*" "\\1" CVC4_VERSION "${CVC4_VERSION}")
 
-  set(CVC4_MIN_VERSION "1.6")
+  set(CVC4_MIN_VERSION "1.8")
   if(CVC4_VERSION VERSION_LESS CVC4_MIN_VERSION)
     message(FATAL_ERROR "Expected version ${CVC4_MIN_VERSION} or greater")
   endif()
