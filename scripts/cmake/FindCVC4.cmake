6b8cb5cd3b48fbe6b75abf6abf1b5f1457b6b831
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2021-05-05 19:38:00 -0400

    Support CVC4
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- scripts/cmake/FindCVC4.cmake
+++ scripts/cmake/FindCVC4.cmake
@@ -1,5 +1,4 @@
-# Looking for CVC4 in SOLVER_CVC4_INCLUDE_DIR
-find_package(CVC4 HINTS ${SOLVER_CVC4_DIR}/lib/cmake/CVC4 $ENV{HOME}/cvc4)
+find_package(CVC4 HINTS ${Camada_CVC4_DIR}/lib/cmake/CVC4 $ENV{HOME}/cvc4)
 
 if(CVC4_FOUND)
   # Remove any suffix from CVC4's version string
@@ -10,21 +9,9 @@ if(CVC4_FOUND)
     message(FATAL_ERROR "Expected version ${CVC4_MIN_VERSION} or greater")
   endif()
 
-  set(SOLVER_CVC4_LIB "CVC4::cvc4")
-
   # Search for symfpu headers and set it a CVC4 include
-  find_path(SOLVER_CVC4_SYMFPU_DIR symfpu/core/unpackedFloat.h PATHS)
-  set(SOLVER_CVC4_INCLUDE_DIR "${SOLVER_CVC4_SYMFPU_DIR}")
+  find_path(CVC4_SYMFPU_DIR symfpu/core/unpackedFloat.h PATHS)
 
-  find_path(GMP_INCLUDE_DIR NAMES gmpxx.h)
-  list(APPEND SOLVER_CVC4_INCLUDE_DIR "${GMP_INCLUDE_DIR}")
-
-  # handle the QUIETLY and REQUIRED arguments and set CVC4_FOUND to TRUE if
-  # all listed variables are TRUE
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(CVC4
-                                    REQUIRED_VARS SOLVER_CVC4_LIB SOLVER_CVC4_INCLUDE_DIR
-                                    VERSION_VAR CVC4_VERSION)
-
-  mark_as_advanced(SOLVER_CVC4_LIB SOLVER_CVC4_INCLUDE_DIR)
+  # TODO: this symfpu check should be clever: we should check if cvc4 was
+  # actually built with symfpu and then abort/warn the user
 endif()
