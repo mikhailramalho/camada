9d8c773627e9a875f0b3585e9df1605cd8e382b2
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2021-05-05 19:25:25 -0400

    Renamed boolector script
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- scripts/cmake/FindBOOLECTOR.cmake
+++
@@ -1,2 +0,0 @@
-# Looking for Boolector in SOLVER_BOOLECTOR_INCLUDE_DIR
-find_package(Boolector HINTS ${SOLVER_BOOLECTOR_DIR}/lib/cmake $ENV{HOME}/boolector)
---
+++ scripts/cmake/FindBoolector.cmake
@@ -0,0 +1,2 @@
+# Looking for Boolector in SOLVER_BOOLECTOR_INCLUDE_DIR
+find_package(Boolector HINTS ${CAMADA_BOOLECTOR_DIR}/lib/cmake $ENV{HOME}/boolector)
