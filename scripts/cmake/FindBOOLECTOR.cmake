04b2366d89d418eabb786d4e80991a427efb3c5a
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2021-05-05 19:13:39 -0400

    Simplify solver check and boolector script
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- scripts/cmake/FindBOOLECTOR.cmake
+++ scripts/cmake/FindBOOLECTOR.cmake
@@ -1,15 +1,2 @@
 # Looking for Boolector in SOLVER_BOOLECTOR_INCLUDE_DIR
 find_package(Boolector HINTS ${SOLVER_BOOLECTOR_DIR}/lib/cmake $ENV{HOME}/boolector)
-
-if(Boolector_FOUND)
-  set(SOLVER_BOOLECTOR_LIB "Boolector::boolector")
-
-  # handle the QUIETLY and REQUIRED arguments and set BOOLECTOR_FOUND to TRUE if
-  # all listed variables are TRUE
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(BOOLECTOR
-                                    REQUIRED_VARS SOLVER_BOOLECTOR_LIB
-                                    VERSION_VAR Boolector_VERSION)
-
-  mark_as_advanced(SOLVER_BOOLECTOR_LIB)
-endif()
