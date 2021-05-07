fdb81347aca68d4ad3d66892aba9ec2df16f6c9b
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2021-05-07 17:53:30 -0400

    Update findboolector script to first search in the dep install path
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- scripts/cmake/FindBoolector.cmake
+++ scripts/cmake/FindBoolector.cmake
@@ -1,2 +1,2 @@
-find_package(Boolector HINTS ${CAMADA_BOOLECTOR_DIR}/lib/cmake
-             $ENV{HOME}/boolector)
+find_package(Boolector HINTS ${CMAKE_SOURCE_DIR}/deps/install/boolector
+             ${CAMADA_BOOLECTOR_DIR}/lib/cmake $ENV{HOME}/boolector)
