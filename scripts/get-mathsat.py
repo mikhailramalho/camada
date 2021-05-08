990f121ac62af91ac759bbf7cf39ad466d672d34
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2021-05-07 21:50:28 -0400

    Added mathsat and z3 missing scripts. ops
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

---
+++ scripts/get-mathsat.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+from common import download_solver_src, run_command, extract_tar_gz, check_root_dir, create_dirs
+import shutil
+
+
+def setup_msat():
+    file_path = download_solver_src("MathSAT 5.6.3",
+                                    "https://mathsat.fbk.eu/download.php?file=mathsat-5.6.6-linux-x86_64.tar.gz",
+                                    "794579f22930e846af7e7a51cfe74df3",
+                                    sep="=")
+    print(file_path)
+    the_dire = file_path.rsplit('/', 1)[0]
+    the_file = file_path.rsplit('/', 1)[1]
+
+    os.chdir("{}".format(the_dire))
+
+    # extracts solver to ./deps/src/mathsat-5.6.6
+    extract_tar_gz(the_file)
+
+    # delete mathsat dir if it exists
+    if os.path.exists('../install/mathsat'):
+        shutil.rmtree('../install/mathsat')
+
+    # move it to deps/install/mathsat
+    shutil.move('./mathsat-5.6.6-linux-x86_64', '../install/mathsat')
+
+
+if __name__ == '__main__':
+    check_root_dir()
+    create_dirs()
+    setup_msat()
---
+++ scripts/get-z3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+from common import download_solver_src, run_command, unzip, check_root_dir, create_dirs
+import shutil
+
+
+def setup_z3():
+    file_path = download_solver_src("Z3 4.8.10",
+                                    "https://github.com/Z3Prover/z3/releases/download/z3-4.8.10/z3-4.8.10-x64-ubuntu-18.04.zip",
+                                    "ab53d3b4b0ef525a62f06f762a441adf")
+    print(file_path)
+    the_dire = file_path.rsplit('/', 1)[0]
+    the_file = file_path.rsplit('/', 1)[1]
+
+    os.chdir("{}".format(the_dire))
+
+    # extracts solver to ./deps/src/z3
+    unzip(the_file)
+
+    # delete z3 dir if it exists
+    if os.path.exists('../install/z3'):
+        shutil.rmtree('../install/z3')
+
+    # move it to deps/install/mathsat
+    shutil.move('./z3-4.8.10-x64-ubuntu-18.04', '../install/z3')
+
+
+if __name__ == '__main__':
+    check_root_dir()
+    create_dirs()
+    setup_z3()
