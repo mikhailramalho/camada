3b432bc44c01b6aa7face197b26211b08666b9f2
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2021-05-13 11:59:17 -0400

    Build minisat statically
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- scripts/solver-deps.py
+++ scripts/solver-deps.py
@@ -71,8 +71,9 @@ def setup_minisat():
     os.mkdir("./build")
     os.chdir("./build")
 
-    run_command(["cmake", "..", "-GNinja",
-                 "-DCMAKE_INSTALL_PREFIX=../../../../install/"])
+    run_command(["cmake", "..", "-GNinja", "-DSTATICCOMPILE=ON", "-DBUILD_SHARED_LIBS=OFF",
+                 "-DCMAKE_INSTALL_PREFIX=../../../install/", "-DCMAKE_BUILD_TYPE=Release",
+                 "-DCMAKE_CXX_FLAGS_RELEASE='-O3 -DNDEBUG -fPIC'"])
     run_command(["ninja"])
     run_command(["ninja", "install"])
 
