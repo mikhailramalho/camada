98ca8d6aaa437f69c743223468b1612d6aa0ac3b
Mikhail R. Gadelha <mikhail@igalia.com>
2023-12-14 12:34:50 -0300

    Add cryptominisat5 dependency to stp
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- scripts/cmake/FindSTP.cmake
+++ scripts/cmake/FindSTP.cmake
@@ -1,2 +1,4 @@
+find_package(cryptominisat5 PATHS ${CMAKE_SOURCE_DIR}/deps/install/)
+
 find_package(STP HINTS ${CMAKE_SOURCE_DIR}/deps/install/
              ${CAMADA_STP_DIR}/lib/cmake $ENV{HOME}/stp)
