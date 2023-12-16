8b4246e31deeefcef9dba25263c53d0b478aa438
Mikhail R. Gadelha <mgadelha@gmail.com>
2023-12-15 22:06:55 -0300

    Try to build z3 instead of downloading a pre built version
    Signed-off-by: Mikhail R. Gadelha <mgadelha@gmail.com>

--- scripts/get-z3.py
+++ scripts/get-z3.py
@@ -8,33 +8,24 @@ from common import download_solver_src, run_command, unzip, check_root_dir, crea
 
 def setup_z3():
     curr_dir = os.getcwd()
-    if sys.platform == "darwin":
-        file_path = download_solver_src("Z3 4.12.4",
-                                        "https://github.com/Z3Prover/z3/releases/download/z3-4.12.4/z3-4.12.4-arm64-osx-11.0.zip")
-    else:
-        file_path = download_solver_src("Z3 4.12.4",
-                                        "https://github.com/Z3Prover/z3/releases/download/z3-4.12.4/z3-4.12.4-x64-glibc-2.35.zip")
+    file_path = download_solver_src("Z3 4.12.4",
+                                    "https://github.com/Z3Prover/z3/archive/refs/tags/z3-4.12.4.zip")
+
     the_dire = file_path.rsplit('/', 1)[0]
     the_file = file_path.rsplit('/', 1)[1]
 
     os.chdir("{}".format(the_dire))
 
-    # extracts solver to ./deps/src/z3
+    # extracts solver to ./deps/src/z3-4.12.4
     unzip(the_file)
 
-    # move it to deps/install/
-    if sys.platform == "darwin":
-        run_command(
-            ["cp", "-r", "./z3-4.12.4-arm64-osx-11.0/bin", "../install/"])
-        run_command(
-            ["cp", "-r", "./z3-4.12.4-arm64-osx-11.0/include", "../install/"])
-    else:
-        run_command(
-            ["cp", "-r", "./z3-4.12.4-x64-glibc-2.35/bin", "../install/"])
-        run_command(
-            ["cp", "-r", "./z3-4.12.4-x64-glibc-2.35/include", "../install/"])
-    os.chdir(curr_dir)
+    os.chdir("./z3-z3-4.12.4")
+    run_command(["python3", "scripts/mk_make.py", "--prefix=../../../install/"])
+    os.chdir("./build")
+    run_command(["make", "-j"])
+    run_command(["make", "install"])
 
+    os.chdir(curr_dir)
 
 if __name__ == '__main__':
     check_root_dir()
