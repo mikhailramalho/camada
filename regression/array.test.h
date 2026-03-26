64d63fb08ffc10fb2613475b456b1dcec879a75c
Mikhail R. Gadelha <mikhail@igalia.com>
2026-03-26 16:40:42 -0300

    Use lambda
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- regression/array.test.h
+++ regression/array.test.h
@@ -33,3 +33,31 @@ inline void array(const camada::SMTSolverRef &solver) {
   REQUIRE(selected->getKind() == camada::SMTExprKind::ArraySelect);
   REQUIRE(solver->getBVInBin(selected) == "0000001010");
 }
+
+inline void array_const_store_semantics(const camada::SMTSolverRef &solver) {
+  auto indexsort = solver->mkBVSort(3);
+  auto elemsort = solver->mkBVSort(8);
+
+  auto init = solver->mkBVFromDec(170, elemsort);
+  auto stored = solver->mkBVFromDec(17, elemsort);
+  auto idx_written = solver->mkBVFromDec(3, indexsort);
+  auto idx_other = solver->mkBVFromDec(5, indexsort);
+
+  auto arr = solver->mkArrayConst(indexsort, init);
+  auto updated = solver->mkArrayStore(arr, idx_written, stored);
+
+  solver->addConstraint(
+      solver->mkEqual(solver->mkArraySelect(updated, idx_written), stored));
+  solver->addConstraint(
+      solver->mkEqual(solver->mkArraySelect(updated, idx_other), init));
+
+  REQUIRE(solver->check() == camada::checkResult::SAT);
+
+  auto read_written = solver->getArrayElement(updated, idx_written);
+  auto read_other = solver->getArrayElement(updated, idx_other);
+
+  REQUIRE(read_written->getKind() == camada::SMTExprKind::ArraySelect);
+  REQUIRE(read_other->getKind() == camada::SMTExprKind::ArraySelect);
+  REQUIRE(solver->getBVInBin(read_written) == "00010001");
+  REQUIRE(solver->getBVInBin(read_other) == "10101010");
+}
