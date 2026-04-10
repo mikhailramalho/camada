9397b0d6f991a2521694c46f750e35f398e2e785
Mikhail R. Gadelha <mikhail@igalia.com>
2026-04-10 11:25:31 -0300

    User fatalError
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- src/camada.cpp
+++ src/camada.cpp
@@ -21,6 +21,7 @@
 
 #include "camada.h"
 #include "ac_config.h"
+#include "camadaerror.h"
 
 #if SOLVER_Z3_ENABLED
 #include "z3solver.h"
@@ -54,9 +55,8 @@ SMTSolverRef createZ3Solver() {
 #if SOLVER_Z3_ENABLED
   return std::make_unique<Z3Solver>();
 #else
-  std::fprintf(stderr, "Camada was not compiled with Z3 support, rebuild with "
-                       "-DCAMADA_ENABLE_SOLVER_Z3=ON\n");
-  abort();
+  fatalError("Camada was not compiled with Z3 support, rebuild with "
+             "-DCAMADA_ENABLE_SOLVER_Z3=ON");
 #endif
 }
 
@@ -64,9 +64,8 @@ SMTSolverRef createMathSATSolver() {
 #if SOLVER_MATHSAT_ENABLED
   return std::make_unique<MathSATSolver>();
 #else
-  std::fprintf(stderr, "Camada was not compiled with MathSAT support, rebuild "
-                       "with -DCAMADA_ENABLE_SOLVER_MATHSAT=ON\n");
-  abort();
+  fatalError("Camada was not compiled with MathSAT support, rebuild with "
+             "-DCAMADA_ENABLE_SOLVER_MATHSAT=ON");
 #endif
 }
 
@@ -74,9 +73,8 @@ SMTSolverRef createCVC5Solver() {
 #if SOLVER_CVC5_ENABLED
   return std::make_unique<CVC5Solver>();
 #else
-  std::fprintf(stderr, "Camada was not compiled with CVC5 support, rebuild "
-                       "with -DCAMADA_ENABLE_SOLVER_CVC5=ON\n");
-  abort();
+  fatalError("Camada was not compiled with CVC5 support, rebuild with "
+             "-DCAMADA_ENABLE_SOLVER_CVC5=ON");
 #endif
 }
 
@@ -84,9 +82,8 @@ SMTSolverRef createBitwuzlaSolver() {
 #if SOLVER_BITWUZLA_ENABLED
   return std::make_unique<BitwuzlaSolver>();
 #else
-  std::fprintf(stderr, "Camada was not compiled with Bitwuzla support, rebuild "
-                       "with -DCAMADA_ENABLE_SOLVER_BITWUZLA=ON\n");
-  abort();
+  fatalError("Camada was not compiled with Bitwuzla support, rebuild with "
+             "-DCAMADA_ENABLE_SOLVER_BITWUZLA=ON");
 #endif
 }
 
@@ -94,9 +91,8 @@ SMTSolverRef createYicesSolver() {
 #if SOLVER_YICES_ENABLED
   return std::make_unique<YicesSolver>();
 #else
-  std::fprintf(stderr, "Camada was not compiled with YICES support, rebuild "
-                       "with -DCAMADA_ENABLE_SOLVER_YICES=ON\n");
-  abort();
+  fatalError("Camada was not compiled with YICES support, rebuild with "
+             "-DCAMADA_ENABLE_SOLVER_YICES=ON");
 #endif
 }
 
@@ -104,9 +100,8 @@ SMTSolverRef createSTPSolver() {
 #if SOLVER_STP_ENABLED
   return std::make_unique<STPSolver>();
 #else
-  std::fprintf(stderr, "Camada was not compiled with STP support, rebuild with "
-                       "-DCAMADA_ENABLE_SOLVER_STP=ON\n");
-  abort();
+  fatalError("Camada was not compiled with STP support, rebuild with "
+             "-DCAMADA_ENABLE_SOLVER_STP=ON");
 #endif
 }
 
